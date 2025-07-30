514c864d56cc6a0e0a844a299ce97570a7bce6f5
yamawade <mariamawade69@gmail.com>
2025-07-30 21:30:14 +0000

    first commit

---
+++ SectorClasseExamen/src/main/java/com/groupeisi/SectorClasseExamen/SectorClasseExamenApplication.java
@@ -0,0 +1,13 @@
+package com.groupeisi.SectorClasseExamen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SectorClasseExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SectorClasseExamenApplication.class, args);
+	}
+
+}
---
+++ SectorClasseExamen/src/test/java/com/groupeisi/SectorClasseExamen/SectorClasseExamenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groupeisi.SectorClasseExamen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SectorClasseExamenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
